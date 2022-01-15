66d8389de59a149af101a7b2397899846b5e1261
Chaitanya Malani <chaitanyamalani.rns@gmail.com>
2022-01-15 15:13:59 +0530

    dependencies removed

--- mail/migrations/0001_initial.py
+++ mail/migrations/0001_initial.py
@@ -12,9 +12,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('auth', '0011_update_proxy_permissions'),
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
